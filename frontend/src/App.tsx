0a86926ae5d1d386c84d0c0463fb8c960c4d155e
Shubham Garg <shubham07119garg@gmail.com>
2024-04-27 11:12:31 +0530

    Adding homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout>HOME PAGE</Layout>} />
+      <Route path="/" element={<Layout><HomePage/></Layout>} />
       <Route path="/user-profile" element={<span>User</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
