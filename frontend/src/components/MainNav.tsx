1d3d9717767d4602facbae7c9a9e14903ae0f5f8
Shubham Garg <shubham07119garg@gmail.com>
2024-05-02 13:09:32 +0530

    Adding login redirects

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,9 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button"
 
 const MainNav = () => {
   return (
-    <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white">
-LogIn
+    <Button 
+      variant="ghost"
+      className="font-bold hover:text-orange-500 hover:bg-white"
+      onClick={async ()=> await loginWithRedirect()}>
+      LogIn
     </Button>
   )
 }
