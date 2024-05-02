2a18eab4792367b48b6fc1233986b049b2d4097e
Shubham Garg <shubham07119garg@gmail.com>
2024-05-02 13:08:00 +0530

    Adding Auth provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <App/>
+      <Auth0ProviderWithNavigate>
+        <App/>
+      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>,
 )
